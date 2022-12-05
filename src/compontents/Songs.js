192c492c481e48098b11a567639b9c34d645d16c
vatchans <112924038+vatchans@users.noreply.github.com>
2022-12-05 21:20:45 +0530

    Update Songs.js

--- src/compontents/Songs.js
+++ src/compontents/Songs.js
@@ -24,7 +24,8 @@ const Songs = {
             mp3: "/songs/Nee-Yenadharuginil-Nee-MassTamilan.fm.mp3",
             artist: "Shakthisree Gopalan, Vishal",
             img: "https://c.saavncdn.com/930/Oh-Manapenne-Tamil-2021-20211019011817-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
         {
             id: "4",
@@ -32,7 +33,8 @@ const Songs = {
             artist: "Anirudh Ravichander, Kamal Haasan",
             mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
             img: "https://c.saavncdn.com/973/Vikram-Tamil-2022-20220515182605-500x500.jpg",
-            genre: "Folk"
+            genre: "Folk",
+            price:44
         },
         {
             id: "5",
@@ -47,209 +49,233 @@ const Songs = {
             id: "6",
             title: "Ennai Vittu",
             artist: " Yuvan Shankar Raja",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Ennai-Vittu-(Yuvanshankar-Raja-Version)-MassTamilan.dev.mp3",
             img: "https://c.saavncdn.com/178/Love-Today-Tamil-2022-20221107113815-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
         {
             id: "7",
             title: "Hosanna",
             artist: "Vijay Prakash, Suzanne D'Mello, A.R. Rahman, Blaaze ft. Suzanne D&#039;Mello",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Hosanna.mp3",
             img: "https://c.saavncdn.com/253/Vinnathaandi-Varuvaayaa-Tamil-2010-20190731134123-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
         {
             id: "8",
             title: "Deadwood",
             artist: "Really Slow Motion",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Deadwood-.mp3",
             img: "https://c.saavncdn.com/519/Deadwood-Unknown-2021-20210503085637-500x500.jpg",
-            price: 44
+            price:44
         },
         {
             id: "9",
             title: "Uyire",
             artist: "Sid Sriram, Ankit Menon",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Uyire-MassTamilan.io.mp3",
             img: "https://c.saavncdn.com/335/Gauthamante-Radham-Malayalam-2020-20211130230913-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
 
         },
         {
             id: "10",
             title: "High on Love",
             artist: "Sid Sriram, Yuvan Shankar Raja",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/High-On-Love-MassTamilan.com.mp3",
             img: "https://c.saavncdn.com/636/Pyaar-Prema-Kaadhal-Unknown-2018-20210530043806-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
 
         },
         {
             id: "11",
             title: "Enna Sona",
             artist: "Arijit Singh ft. A.R. Rahman & Arijit Singh",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/320kbps_OJ - Enna Sona.mp3",
             img: "https://c.saavncdn.com/835/OK-Jaanu-Hindi-2017-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
 
         },
         {
             id: "12",
             title: "Baby",
             artist: "Justin Bieber",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Baby_192(PagalWorld).mp3",
             img: "https://c.saavncdn.com/728/My-World-2-0-English-2010-20200606003742-500x500.jpg",
+            price:44
 
         },
         {
             id: "13",
             title: "Beliver",
             artist: "Imagine Dragons",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Imagine_Dragons_-_Believer__(Jesusful.com).mp3",
             img: "https://c.saavncdn.com/248/Evolve-English-2017-20180716230950-500x500.jpg",
-            genre: "rap"
+            genre: "rap",
+            price:44
 
         },
         {
             id: "14",
             title: "One More Night",
             artist: "Maroon 5",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Maroon_5_-_One_More_Night_(Jesusful.com).mp3",
             img: "https://i.scdn.co/image/ab67616d0000b273ce7d499847da02a9cbd1c084",
-            genre: "rap"
+            genre: "rap",
+            price:44
 
         },
         {
             id: "15",
             title: "Rap God",
             artist: "Eminem",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Rap-God---Eminem-320(PagalWorld).mp3",
             img: "https://c.saavncdn.com/321/Rap-God-2013-500x500.jpg",
-            genre: "rap"
+            genre: "rap",
+            price:44
 
         },
         {
             id: "16",
             title: "STAY",
             artist: "The Kid LAROI & Justin Bieber",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Stay(PagalWorld).mp3",
             img: "https://c.saavncdn.com/895/Stay-English-2021-20210706223809-500x500.jpg",
-            genre: "rap"
+            genre: "rap",
+            price:44
 
         },
         {
             id: "17",
             title: "Closer",
             artist: "The Chainsmokers ft. Halsey",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Closer - The Chainsmokers_128-(DJMaza).mp3",
             img: "https://c.saavncdn.com/147/Closer-English-2016-500x500.jpg",
-            genre: "pop"
+            genre: "pop",
+            price:44
         },
         {
             id: "18",
             title: "Adiye",
             artist: "G.V. Prakash Kumar,Dhibu Ninan Thomas, Kapil Kapilan",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Adiye-MassTamilan.fm.mp3",
             img: "https://c.saavncdn.com/357/Bachelor-Tamil-2020-20220907150805-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
 
         {
             id: "19",
             title: "Yathe Yathe",
             artist: "G.V. Prakash Kumar,Dhibu Ninan Thomas, Kapil Kapilan",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Yathe-Yathe.mp3",
             img: "https://c.saavncdn.com/682/Aadukalam-Tamil-2010-20190731133223-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
         {
             id: "20",
             title: "Yaar Indha Saalai Oram",
             artist: "G.V. Prakash Kumar, Saindhavi",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Yaar-Indha-Saalai-Oram.mp3",
             img: "https://c.saavncdn.com/026/Thalaivaa-Tamil-2013-20190822135524-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
         {
             id: "21",
             title: "Vaathi Raid",
             artist: "Anirudh Ravichander, Arivu",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Vaathi Raid Master 128 Kbps.mp3",
             img: "https://c.saavncdn.com/347/Master-Tamil-2020-20200316084627-500x500.jpg",
-            genre: "rap"
+            genre: "rap",
+            price:44
         },
         {
             id: "22",
             title: "Ethir Neechal",
             artist: "Anirudh Ravichander",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Ethir-Neechal.mp3",
             img: "https://c.saavncdn.com/259/Ethir-Neechal-2012-500x500.jpg",
-            genre: "rap"
+            genre: "rap",
+            price:44
         },
         {
             id: "23",
             title: "Yaakkai Thiri",
             artist: "A.R. Rahman, Sunitha Sarathy, Pop Shalini",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Yakkai-Thiri.mp3",
             img: "https://c.saavncdn.com/929/Aayitha-Ezhutu-Tamil-2004-20190629140126-500x500.jpg",
-            genre: "pop"
+            genre: "pop",
+            price:44
         },
         {
             id: "24",
             title: "Mental Manadhil",
             artist: "A.R. Rahman, Sunitha Sarathy, Pop Shalini",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Mental-Manadhil.mp3",
             img: "https://c.saavncdn.com/336/O-Kadhal-Kanmani-Tamil-2015-20200805153450-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
         {
             id: "25",
             title: "Munbe Vaa",
             artist: "A.R. Rahman,Shreya Ghoshal",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Munbe-Vaa-En-Vaa.mp3",
             img: "https://c.saavncdn.com/106/Jillunu-Oru-Kadhal-2006-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
         {
             id: "26",
             title: "Needhane",
             artist: "Yuvan Shankar Raja",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Neethane.mp3",
             img: "https://c.saavncdn.com/191/Sarvam-Tamil-2009-20220915023508-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
         {
             id: "27",
             title: "Thuli Thuli",
             artist: "Yuvan Shankar Raja",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Thuli-Thuli-Mazhaiyaai.mp3",
             img: "https://c.saavncdn.com/984/Paiya-Tamil-2010-20200620134043-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
         {
             id: "28",
             title: "Senjitaley",
             artist: "Anirudh Ravichander",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Senjitaley.mp3",
             img: "https://c.saavncdn.com/110/Remo-Tamil-2016-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
         {
             id: "29",
-            title: "Senjitaley",
-            artist: "Anirudh Ravichander",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
-            img: "https://c.saavncdn.com/110/Remo-Tamil-2016-500x500.jpg",
-            genre: "melody"
+            title: "Thee Thalapathy",
+            artist: "Silambarasan TR, Thaman S",
+            mp3: "/songs/Thee-Thalapathy-MassTamilan.dev.mp3",
+            img: "https://c.saavncdn.com/006/Thee-Thalapathy-From-Varisu-Tamil-2022-20221205181511-500x500.jpg",
+            genre: "melody",
+            price:44
         },
         {
             id: "30",
             title: "Gundellonaa",
             artist: "Anirudh Ravichander",
-            mp3: "/songs/Pathala-Pathala-MassTamilan.so.mp3",
+            mp3: "/songs/Gundellonaa-(Ori-Devuda)(PagalWorldl).mp3",
             img: "https://c.saavncdn.com/901/Ori-Devuda-Telugu-2022-20221027144002-500x500.jpg",
-            genre: "melody"
+            genre: "melody",
+            price:44
         },
 
     ]
