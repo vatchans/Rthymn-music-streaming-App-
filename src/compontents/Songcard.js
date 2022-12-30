da17e41ad720cb552267a0b242de93bc835b2575
vatchans <112924038+vatchans@users.noreply.github.com>
2022-12-30 14:26:27 +0530

    Update Songcard.js

--- src/compontents/Songcard.js
+++ src/compontents/Songcard.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 import { useCart } from "react-use-cart";
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+import Heart from "react-heart"
+import { ToastContainer, toast } from 'react-toastify';
 function Songcard(props) {
     const { addItem } = useCart();
-    return <> 
-    <div className="col movie" key={props.id}>
+    const [active, setActive] = useState(false)
+    const notify = () => {
+        toast.success('Song Added to your Playlist', {
+            toastId: 'success1',
+        })
+    }
+    return <>
+        <div className="col movie" key={props.id}>
             <div className="card" style={{ width: "18rem;" }}>
 
                 <img src={props.img} className="card-img-top mx-auto mt-3 ml-5 img-rounded" alt="..." />
 
                 <div className="card-body">
-                    <div><i className="fa-solid fa-heart" onClick={() => addItem(props.item)}></i>
-                    </div>
+                    <div className='heart'>
+                        <Heart isActive={active} onClick={() =>{{notify()}
+                            setActive(!active)
+                            addItem(props.item)}} />
+                            <ToastContainer closeButton={false} autoClose={500} />
+                             </div>
                     <h5 className="card-title text-center">{props.title}</h5>
 
                     <marquee Scrolldelay={120}>
@@ -20,13 +33,13 @@ function Songcard(props) {
                     </marquee>
                     <div id="playbar">
                         <AudioPlayer
-                          
+
                             src={props.mp3} />
                     </div>
                 </div>
             </div>
-            </div>
-        </>
+        </div>
+    </>
 }
 
-        export default Songcard
+export default Songcard
